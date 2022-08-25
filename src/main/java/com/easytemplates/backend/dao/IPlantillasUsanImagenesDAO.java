3c8ed7d4db7078de7d5c9bc494447e7765153775
Adrià Milan <adria4milan@gmail.com>
2022-08-25 22:38:03 +0200

    Plantillas Usan Imagenes 1.1

---
+++ src/main/java/com/easytemplates/backend/dao/IPlantillasUsanImagenesDAO.java
@@ -0,0 +1,9 @@
+package com.easytemplates.backend.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.easytemplates.backend.dto.PlantillasUsanImagenes;
+
+public interface IPlantillasUsanImagenesDAO extends JpaRepository<PlantillasUsanImagenes, Long> {
+
+}
