10b96cca9faec758dd0e4a8469b2994bb3caf663
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-08-26 11:58:37 +0200

    Controller: GrupoTieneDocs Basic CRUD

--- src/main/java/com/easytemplates/backend/controller/GrupoTieneDocsController.java
+++ src/main/java/com/easytemplates/backend/controller/GrupoTieneDocsController.java
@@ -1,9 +1,18 @@
 package com.easytemplates.backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.easytemplates.backend.dto.GrupoTieneDocs;
 import com.easytemplates.backend.service.GrupoTieneDocsServiceImpl;
 
 @RestController
@@ -12,4 +21,35 @@ public class GrupoTieneDocsController {
 
 	@Autowired
 	GrupoTieneDocsServiceImpl gtdCtl;
+	
+	@GetMapping("/grupostienendocs")
+	public List<GrupoTieneDocs> getAllGrupoTieneDocs() {
+		return gtdCtl.listAllGrupoTieneDocs();
+	}
+	
+	@PostMapping("/grupostienendocs")
+	public GrupoTieneDocs saveGrupoTieneDocs(@RequestBody GrupoTieneDocs g) {
+		return gtdCtl.saveGrupoTieneDocs(g);
+	}
+	
+	@GetMapping("/grupostienendocs/{id}")
+	public GrupoTieneDocs getGrupoTieneDocsById(@PathVariable(name = "id") Long id) {
+		return gtdCtl.grupoTieneDocsById(id);
+	}
+	
+	@PutMapping("/grupostienendocs/{id}")
+	public GrupoTieneDocs updateGrupoTieneDocs(@PathVariable(name = "id") Long id, @RequestBody GrupoTieneDocs g) {
+		GrupoTieneDocs updatedGrupoTieneDocs;
+
+		updatedGrupoTieneDocs = gtdCtl.grupoTieneDocsById(id);
+
+		gtdCtl.saveGrupoTieneDocs(updatedGrupoTieneDocs);
+
+		return updatedGrupoTieneDocs;
+	}
+	
+	@DeleteMapping("/grupostienendocs/{id}")
+	public void deleteGrupoTieneDocs(@PathVariable(name = "id") Long id) {
+		gtdCtl.deleteGrupoTieneDocs(id);
+	}
 }
