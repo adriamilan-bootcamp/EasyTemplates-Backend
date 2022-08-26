a41e63f913072d59ad50d4ad91d55822c965d66f
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-08-26 10:39:56 +0200

    Fix projec structure; DTOs come from G9

---
+++ src/main/java/com/easytemplates/backend/dao/IPlantillaUsaImagenesDAO.java
@@ -0,0 +1,9 @@
+package com.easytemplates.backend.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.easytemplates.backend.dto.PlantillaUsaImagenes;
+
+public interface IPlantillaUsaImagenesDAO extends JpaRepository<PlantillaUsaImagenes, Long> {
+
+}
