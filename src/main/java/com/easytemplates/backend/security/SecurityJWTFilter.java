c46ecfb83aeadf9bf7a5bcca105beba8421a41db
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-08-31 20:51:02 +0200

    Security: Implement the *actual* filtering

--- src/main/java/com/easytemplates/backend/security/SecurityJWTFilter.java
+++ src/main/java/com/easytemplates/backend/security/SecurityJWTFilter.java
@@ -6,7 +6,9 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
 public class SecurityJWTFilter extends GenericFilterBean {
@@ -14,8 +16,15 @@ public class SecurityJWTFilter extends GenericFilterBean {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		// TODO Auto-generated method stub
 		
+		// Grab the authentication information from the HTTP Request
+		Authentication authentication = SecurityJWTUtil.getAuthentication((HttpServletRequest) request);
+
+		// Setup a new SecurityContext w/that authentication information
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		// Apply the request filtering
+		chain.doFilter(request, response);
 	}
 
 }
