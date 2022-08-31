fc52bdc7866ed2ce1bee456cccf5e3563477b6a2
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-08-31 20:56:32 +0200

    Security: LoginFilter progress; implements done and skeleton missing iface. methods scoped in

--- src/main/java/com/easytemplates/backend/security/SecurityLoginFilter.java
+++ src/main/java/com/easytemplates/backend/security/SecurityLoginFilter.java
@@ -1,5 +1,39 @@
 package com.easytemplates.backend.security;
 
-public class SecurityLoginFilter {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+public class SecurityLoginFilter extends AbstractAuthenticationProcessingFilter {
+	
+	/**
+	 * 	Constructor for the Filter; we use super() w/our URL arg. to setup a cascading
+	 * 	parent object and we them set the authManager
+	 * 
+	 * 	@param url			The URL to process
+	 * 	@param authManager	The authentication manager obj.
+	 */
+	public SecurityLoginFilter(String url, AuthenticationManager authManager) {
+		
+		// Call to the parent class
+        super(new AntPathRequestMatcher(url));
+        
+        // Set the authManager obj.
+        setAuthenticationManager(authManager);
+    }
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException, IOException, ServletException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
