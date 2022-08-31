136dcbf34a05c091606b5d23359dc65c3d064da5
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-08-31 21:02:03 +0200

    Security: Filter implemented

--- src/main/java/com/easytemplates/backend/security/SecurityLoginFilter.java
+++ src/main/java/com/easytemplates/backend/security/SecurityLoginFilter.java
@@ -1,16 +1,23 @@
 package com.easytemplates.backend.security;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class SecurityLoginFilter extends AbstractAuthenticationProcessingFilter {
 	
@@ -33,7 +40,30 @@ public class SecurityLoginFilter extends AbstractAuthenticationProcessingFilter 
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
 			throws AuthenticationException, IOException, ServletException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		// Extract the HTTP Request's body
+        InputStream requestBody = request.getInputStream();
+
+        // Map the request's JSON user and password
+        User user = new ObjectMapper().readValue(requestBody, User.class);
+
+        // Do the authentication
+        return getAuthenticationManager().authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        user.getName(),
+                        user.getPassword(),
+                        Collections.emptyList()
+                )
+        );
 	}
+	
+	@Override
+    protected void successfulAuthentication(
+            HttpServletRequest req,
+            HttpServletResponse res, FilterChain chain,
+            Authentication auth) throws IOException, ServletException {
+
+        // Add the JWT into the response
+        SecurityJWTUtil.addAuthentication(res, auth.getName());
+    }
 }
