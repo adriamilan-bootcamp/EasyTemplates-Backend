99e15ac3a89393600522626e5281a88087746012
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-08-31 20:47:46 +0200

    Security: Filter needs to extend the GenericFilterBean interface

--- src/main/java/com/easytemplates/backend/security/SecurityJWTFilter.java
+++ src/main/java/com/easytemplates/backend/security/SecurityJWTFilter.java
@@ -1,5 +1,21 @@
 package com.easytemplates.backend.security;
 
-public class SecurityJWTFilter {
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+public class SecurityJWTFilter extends GenericFilterBean {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
