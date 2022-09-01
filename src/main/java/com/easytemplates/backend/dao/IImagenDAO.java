f565467836ec9102f5a122efba5120ec13c7209c
Adrià Milan <adria4milan@gmail.com>
2022-09-01 13:39:48 +0200

    Ionela Changes

--- src/main/java/com/easytemplates/backend/dao/IImagenDAO.java
+++
@@ -1,9 +0,0 @@
-package com.easytemplates.backend.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.easytemplates.backend.dto.Imagen;
-
-public interface IImagenDAO extends JpaRepository<Imagen, Long>{
-
-}
--- src/main/java/com/easytemplates/backend/dao/IPlantillaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.easytemplates.backend.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.easytemplates.backend.dto.Plantilla;
-
-public interface IPlantillaDAO extends JpaRepository<Plantilla, Long> {
-
-}
