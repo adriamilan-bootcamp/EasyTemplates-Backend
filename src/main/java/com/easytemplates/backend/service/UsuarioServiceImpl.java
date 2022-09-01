186777e1361d7a041c64e3e2694fba221b709454
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-09-01 19:26:01 +0200

    Service: Implement Security shenanigans in svc layer

--- src/main/java/com/easytemplates/backend/service/UsuarioServiceImpl.java
+++ src/main/java/com/easytemplates/backend/service/UsuarioServiceImpl.java
@@ -3,13 +3,18 @@ package com.easytemplates.backend.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.easytemplates.backend.dao.IUsuarioDAO;
 import com.easytemplates.backend.dto.Usuarios;
+import static java.util.Collections.emptyList;
 
 @Service
-public class UsuarioServiceImpl implements IUsuarioService {
+public class UsuarioServiceImpl implements IUsuarioService,UserDetailsService {
 	@Autowired
 	IUsuarioDAO usuarioDAO;
 
@@ -36,6 +41,20 @@ public class UsuarioServiceImpl implements IUsuarioService {
 	@Override
 	public void deleteUsuario(Long id) {
 		usuarioDAO.deleteById(id);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuarios user = usuarioDAO.findByNombre(username);
+		if (user == null) {
+			throw new UsernameNotFoundException(username);
+		}
+		return new User(user.getNombre(), user.getPassword(), emptyList());
+	}
+
+	@Override
+	public Usuarios usuarioByNombre(String username) {
+		return usuarioDAO.findByNombre(username);
 	}
 
 }
