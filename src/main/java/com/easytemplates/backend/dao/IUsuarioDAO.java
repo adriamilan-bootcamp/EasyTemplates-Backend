171ae5f48688998eb0509315cfe1caa8d4dd832c
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-09-01 19:21:30 +0200

    DAO/Usuario: Add auxiliar method to handle JWT shenanigans

--- src/main/java/com/easytemplates/backend/dao/IUsuarioDAO.java
+++ src/main/java/com/easytemplates/backend/dao/IUsuarioDAO.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.easytemplates.backend.dto.Usuarios;
 public interface IUsuarioDAO extends JpaRepository<Usuarios, Long> {
-	
+	Usuarios findByNombre(String username);
 }
