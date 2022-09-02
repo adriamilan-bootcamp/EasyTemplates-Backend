97f964c5d5aaecce6bd2936bb8a861ff43c9872f
vladasel <ionelavladasel2710@gmail.com>
2022-09-02 10:18:52 +0200

    controller new tables crud basic

---
+++ src/main/java/com/easytemplates/backend/controller/GrupoPdfController.java
@@ -0,0 +1,66 @@
+package com.easytemplates.backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.easytemplates.backend.dto.GruposPdfs;
+import com.easytemplates.backend.service.GrupoPdfServiceImpl;
+
+
+
+@RestController
+@RequestMapping("/api")
+public class GrupoPdfController {
+	@Autowired
+	GrupoPdfServiceImpl serviceImpl;
+	
+	@GetMapping("/grupos_plantillas")
+	public List<GruposPdfs> listarGruposPdfs() {
+		return serviceImpl.listAllGruposPdfs();
+	}
+
+	@PostMapping("/grupos_plantillas")
+	public GruposPdfs salvarGruposPdfs(@RequestBody GruposPdfs grupo_pdf) {
+
+		return serviceImpl.saveGruposPdfs(grupo_pdf);
+	}
+
+	@GetMapping("/grupos_plantillas/{id}")
+	public GruposPdfs gruposPdfsXID(@PathVariable(name = "id") Long id) {
+
+		return serviceImpl.gruposPdfsById(id);
+	}
+
+	@PutMapping("/grupos_plantillas/{id}")
+	public GruposPdfs actualizarGruposPdfs(@PathVariable(name = "id") Long id, @RequestBody GruposPdfs grupo_pdf) {
+
+		GruposPdfs seleccionado = new GruposPdfs();
+		GruposPdfs actualizado = new GruposPdfs();
+
+		seleccionado = serviceImpl.gruposPdfsById(id);
+
+		seleccionado.setGrupos(grupo_pdf.getGrupos());
+		seleccionado.setPdfs(grupo_pdf.getPdfs());
+		
+		
+
+		actualizado = serviceImpl.updateGruposPdfs(seleccionado);
+
+		return actualizado;
+	}
+
+	@DeleteMapping("/grupos_plantillas/{id}")
+
+	public void eliminarGruposPdfs(@PathVariable(name = "id") Long id) {
+		serviceImpl.deleteGruposPdfs(id);
+	}
+}
