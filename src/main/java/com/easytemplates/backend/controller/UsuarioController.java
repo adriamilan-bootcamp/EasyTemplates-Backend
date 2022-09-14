2a8a9e54ce35daa07655b0f0811f1f3ee9f9bdb8
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-09-14 21:56:36 +0200

    Controller: Properly separate privileges on the user controller

--- src/main/java/com/easytemplates/backend/controller/UsuarioController.java
+++ src/main/java/com/easytemplates/backend/controller/UsuarioController.java
@@ -1,10 +1,15 @@
 package com.easytemplates.backend.controller;
 
+import java.util.HashMap;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,31 +42,57 @@ public class UsuarioController {
 	}
 
 	@GetMapping("/usuarios/{id}")
-	public Usuarios usuarioXID(@PathVariable(name = "id") Long id) {
-
-		return usuarioServiceImpl.usuarioById(id);
+	public ResponseEntity<Object> usuarioXID(@PathVariable(name = "id") Long id) {
+		
+		Usuarios user = usuarioServiceImpl.usuarioById(id);
+		
+		if (user != null)
+		{
+			// Simple way to build a JSON Object
+			HashMap<String, String> userInfo = new HashMap<>();
+			
+			// Non-admin clients don't need more information
+			userInfo.put("email", user.getEmail());
+			userInfo.put("username", user.getNombre());
+		    
+			return new ResponseEntity<Object>(userInfo, HttpStatus.UNAUTHORIZED);
+		}
+	
+		return new ResponseEntity<Object>("The specified user and it\'s doesn\'t exist in the database, try registering it", HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 	@PutMapping("/usuarios/{id}")
-	public Usuarios actualizarUsuario(@PathVariable(name = "id") Long id, @RequestBody Usuarios usuario) {
-
-		Usuarios seleccionado = new Usuarios();
-		Usuarios actualizado = new Usuarios();
-
-		seleccionado = usuarioServiceImpl.usuarioById(id);
-
-		seleccionado.setNombre(usuario.getNombre());
-		seleccionado.setEmail(usuario.getEmail());
-		seleccionado.setPassword(usuario.getPassword());
+	public ResponseEntity<Object> actualizarUsuario(@PathVariable(name = "id") Long id, @RequestBody UserDetailsRequestModel requestUserDetails) {
+		Usuarios user = usuarioServiceImpl.usuarioById(id);
 		
-
-		actualizado = usuarioServiceImpl.updateUsuario(seleccionado);
-
-		return actualizado;
+		if (user != null)
+		{
+			String targetUserEmail = usuarioServiceImpl.usuarioById(id).getEmail();
+			String authedUserEmail = ((Usuarios) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getEmail();
+			
+			
+			if (targetUserEmail.equals(authedUserEmail))
+			{
+				String encoded = bCryptPasswordEncoder.encode(requestUserDetails.getPassword());
+				
+				user.setNombre(requestUserDetails.getNombre());
+				user.setEmail(requestUserDetails.getEmail());
+				user.setPassword(encoded);
+				
+				usuarioServiceImpl.updateUsuario(user);
+				
+				return new ResponseEntity<Object>(user, HttpStatus.OK);
+			}
+			else
+			{
+				return new ResponseEntity<Object>("You are not authorized to edit other user\'s acounts", HttpStatus.UNAUTHORIZED);
+			}
+		}
+	
+		return new ResponseEntity<Object>("The specified user and it\'s doesn\'t exist in the database, try registering it", HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 	@DeleteMapping("/usuarios/{id}")
-
 	public void eliminarUsuario(@PathVariable(name = "id") Long id) {
 		usuarioServiceImpl.deleteUsuario(id);
 	}
