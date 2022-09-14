1e18d1d5d21376f6de99fefbded9543053f81723
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-09-14 22:48:46 +0200

    UserController: Admin and non-admins now have different results

--- src/main/java/com/easytemplates/backend/controller/UsuarioController.java
+++ src/main/java/com/easytemplates/backend/controller/UsuarioController.java
@@ -1,7 +1,10 @@
 package com.easytemplates.backend.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.easytemplates.backend.dto.Role;
 import com.easytemplates.backend.dto.Usuarios;
 import com.easytemplates.backend.service.UsuarioServiceImpl;
 
@@ -48,14 +52,32 @@ public class UsuarioController {
 		
 		if (user != null)
 		{
-			// Simple way to build a JSON Object
-			HashMap<String, String> userInfo = new HashMap<>();
+			// Find current user roles
+			Set<Role> userRoles = ((Usuarios) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getRoles();
 			
-			// Non-admin clients don't need more information
-			userInfo.put("email", user.getEmail());
-			userInfo.put("username", user.getNombre());
-		    
-			return new ResponseEntity<Object>(userInfo, HttpStatus.UNAUTHORIZED);
+			HashSet<String> userRolesArray = new HashSet<String>(userRoles.size());
+			
+			// Map each role name to a Strings HashSet
+			for(Role role : userRoles) {
+				userRolesArray.add(role.toString());
+			}
+			
+			// To decide wether or not display the entire user information, check if HashSet contains ROLE_ADMIN
+			if (userRolesArray.contains("ROLE_ADMIN") == true)
+			{
+				return new ResponseEntity<Object>(user, HttpStatus.OK);
+			}
+			else
+			{
+				// Simple way to build a JSON Object
+				HashMap<String, String> userInfo = new HashMap<>();
+				
+				// Non-admin clients don't need more information
+				userInfo.put("email", user.getEmail());
+				userInfo.put("username", user.getNombre());
+			    
+				return new ResponseEntity<Object>(userInfo, HttpStatus.OK);	
+			}
 		}
 	
 		return new ResponseEntity<Object>("The specified user doesn\'t exist in the database, try registering it", HttpStatus.INTERNAL_SERVER_ERROR);
