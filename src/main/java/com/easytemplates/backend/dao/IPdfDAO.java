4ea52a3e642d5fe50b8d76761f38cf1a9003d043
vladasel <ionelavladasel2710@gmail.com>
2022-09-22 01:35:27 +0200

    add method in pdfDAO

--- src/main/java/com/easytemplates/backend/dao/IPdfDAO.java
+++ src/main/java/com/easytemplates/backend/dao/IPdfDAO.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.easytemplates.backend.dto.Pdfs;
 
 public interface IPdfDAO extends JpaRepository<Pdfs, Long> {
-	
+	<List>Pdfs findByTitulo(String titulo);
 }
