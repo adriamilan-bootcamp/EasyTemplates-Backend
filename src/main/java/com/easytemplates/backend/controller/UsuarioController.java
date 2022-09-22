9327bef3cdb32c03a33617ee8b61c65ec777772f
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-09-22 11:12:21 +0200

    UserCtl: Custom methods that return roles too

--- src/main/java/com/easytemplates/backend/controller/UsuarioController.java
+++ src/main/java/com/easytemplates/backend/controller/UsuarioController.java
@@ -24,8 +24,8 @@ import com.easytemplates.backend.dto.Role;
 import com.easytemplates.backend.dto.Usuarios;
 import com.easytemplates.backend.service.UsuarioServiceImpl;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 @RestController
 @RequestMapping("/api")
@@ -42,27 +42,27 @@ public class UsuarioController {
 	public UsuarioController(BCryptPasswordEncoder bCryptPasswordEncoder) {
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
-	
+
 	@GetMapping(value = "/usuarios", produces = MediaType.APPLICATION_JSON_VALUE)
 	public String listarUsuarios() {
 		List<Usuarios> user = usuarioServiceImpl.listAllUsuarios();
 
 		JsonObject json = new JsonObject();
-		JsonArray   array   = new JsonArray ();
+		JsonArray array = new JsonArray();
 
-		Set<Role> userRoles = ((Usuarios) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getRoles();
+		Set<Role> userRoles = ((Usuarios) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+				.getRoles();
 
 		boolean admin = false;
 
 		HashSet<String> userRolesArray = new HashSet<String>(userRoles.size());
 
 		// Map each role name to a Strings HashSet
-		for(Role role : userRoles) {
+		for (Role role : userRoles) {
 			userRolesArray.add(role.toString());
 		}
 
-		if (userRolesArray.contains("ROLE_ADMIN") == true)
-		{
+		if (userRolesArray.contains("ROLE_ADMIN") == true) {
 			admin = true;
 		}
 
@@ -73,12 +73,9 @@ public class UsuarioController {
 			json.addProperty("firma", user.get(i).getFirma());
 			json.addProperty("password", user.get(i).getPassword());
 
-			if (admin == true)
-			{
-				json.addProperty("roles", user.get(i).getAuthorities().toString()
-						.replace("[", "")
-					    .replace("]", "")
-					    .replace("ROLE_", ""));
+			if (admin == true) {
+				json.addProperty("roles",
+						user.get(i).getAuthorities().toString().replace("[", "").replace("]", "").replace("ROLE_", ""));
 			}
 
 			array.add(gson.toJsonTree(json));
@@ -90,91 +87,175 @@ public class UsuarioController {
 	}
 
 	@GetMapping("/usuarios/{id}")
-	public ResponseEntity<Object> usuarioXID(@PathVariable(name = "id") Long id) {
-		
-		Usuarios user = usuarioServiceImpl.usuarioById(id);
-		
-		if (user != null)
-		{
-			return new ResponseEntity<Object>(user, HttpStatus.OK);
-		}
-		
-		return new ResponseEntity<Object>("The specified user doesn\'t exist in the database, try registering it", HttpStatus.INTERNAL_SERVER_ERROR);
-	}
-	
-	@GetMapping("/usuarios/name/{nombre}")
-	public List<Usuarios> usuarioXNombre(@PathVariable(name = "nombre") String nombre) {
+	public String usuarioXID(@PathVariable(name = "id") Long id) {
 
-		return iUsuarioDAO.findByUsername	(nombre);
+		Usuarios user = usuarioServiceImpl.usuarioById(id);
+
+		JsonObject json = new JsonObject();
+		Set<Role> userRoles = ((Usuarios) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+				.getRoles();
+
+		HashSet<String> userRolesArray = new HashSet<String>(userRoles.size());
+
+		// Map each role name to a Strings HashSet
+		for (Role role : userRoles) {
+			userRolesArray.add(role.toString());
+		}
+
+		json.addProperty("id", String.valueOf(user.getId()));
+		json.addProperty("username", user.getUsername());
+		json.addProperty("email", user.getEmail());
+		json.addProperty("firma", user.getFirma());
+		json.addProperty("password", user.getPassword());
+
+		if (userRolesArray.contains("ROLE_ADMIN") == true) {
+			json.addProperty("roles",
+					user.getAuthorities().toString().replace("[", "").replace("]", "").replace("ROLE_", ""));
+		}
+
+		String userJsonString = this.gson.toJson(json);
+
+		return userJsonString;
+	}
+
+	@GetMapping("/usuarios/name/{nombre}")
+	public String usuarioXNombre(@PathVariable(name = "nombre") String nombre) {
+
+		List<Usuarios> user = iUsuarioDAO.findByUsername(nombre);
+
+		JsonObject json = new JsonObject();
+		JsonArray array = new JsonArray();
+
+		Set<Role> userRoles = ((Usuarios) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+				.getRoles();
+
+		boolean admin = false;
+
+		HashSet<String> userRolesArray = new HashSet<String>(userRoles.size());
+
+		// Map each role name to a Strings HashSet
+		for (Role role : userRoles) {
+			userRolesArray.add(role.toString());
+		}
+
+		if (userRolesArray.contains("ROLE_ADMIN") == true) {
+			admin = true;
+		}
+
+		for (int i = 0; i < user.size(); i++) {
+			json.addProperty("id", String.valueOf(user.get(i).getId()));
+			json.addProperty("username", user.get(i).getUsername());
+			json.addProperty("email", user.get(i).getEmail());
+			json.addProperty("firma", user.get(i).getFirma());
+			json.addProperty("password", user.get(i).getPassword());
+
+			if (admin == true) {
+				json.addProperty("roles",
+						user.get(i).getAuthorities().toString().replace("[", "").replace("]", "").replace("ROLE_", ""));
+			}
+
+			array.add(gson.toJsonTree(json));
+		}
+
+		String userJsonString = this.gson.toJson(array);
+
+		return userJsonString;
 	}
 
 	@GetMapping("/usuarios/email/{email}")
-	public Usuarios usuarioXemail(@PathVariable(name = "email") String email) {
+	public String usuarioXemail(@PathVariable(name = "email") String email) {
 
-		return iUsuarioDAO.findByEmail(email);
+		Usuarios user = iUsuarioDAO.findByEmail(email);
+		
+		JsonObject json = new JsonObject();
+		Set<Role> userRoles = ((Usuarios) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+				.getRoles();
+
+		HashSet<String> userRolesArray = new HashSet<String>(userRoles.size());
+
+		// Map each role name to a Strings HashSet
+		for (Role role : userRoles) {
+			userRolesArray.add(role.toString());
+		}
+
+		json.addProperty("id", String.valueOf(user.getId()));
+		json.addProperty("username", user.getUsername());
+		json.addProperty("email", user.getEmail());
+		json.addProperty("firma", user.getFirma());
+		json.addProperty("password", user.getPassword());
+
+		if (userRolesArray.contains("ROLE_ADMIN") == true) {
+			json.addProperty("roles",
+					user.getAuthorities().toString().replace("[", "").replace("]", "").replace("ROLE_", ""));
+		}
+
+		String userJsonString = this.gson.toJson(json);
+
+		return userJsonString;
 	}
 
 	/**
-	 *  The user will have to login again (JWT Bearer Token is invalid after new user details)
+	 * The user will have to login again (JWT Bearer Token is invalid after new user
+	 * details)
 	 */
 	@PutMapping("/usuarios/{id}")
-	public ResponseEntity<Object> actualizarUsuario(@PathVariable(name = "id") Long id, @RequestBody UserDetailsRequestModel requestUserDetails) {
+	public ResponseEntity<Object> actualizarUsuario(@PathVariable(name = "id") Long id,
+			@RequestBody UserDetailsRequestModel requestUserDetails) {
 		Usuarios user = usuarioServiceImpl.usuarioById(id);
-		
-		if (user != null)
-		{
+
+		if (user != null) {
 			String targetUserEmail = usuarioServiceImpl.usuarioById(id).getEmail();
-			String authedUserEmail = ((Usuarios) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getEmail();
-			
-			Set<Role> userRoles = ((Usuarios) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getRoles();
-			
+			String authedUserEmail = ((Usuarios) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+					.getEmail();
+
+			Set<Role> userRoles = ((Usuarios) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+					.getRoles();
+
 			HashSet<String> userRolesArray = new HashSet<String>(userRoles.size());
-			
+
 			// Map each role name to a Strings HashSet
-			for(Role role : userRoles) {
+			for (Role role : userRoles) {
 				userRolesArray.add(role.toString());
 			}
-			
-			if (targetUserEmail.equals(authedUserEmail) || userRolesArray.contains("ROLE_ADMIN") == true)
-			{
+
+			if (targetUserEmail.equals(authedUserEmail) || userRolesArray.contains("ROLE_ADMIN") == true) {
 				String encoded = bCryptPasswordEncoder.encode(requestUserDetails.getPassword());
-				
+
 				user.setNombre(requestUserDetails.getNombre());
 				user.setEmail(requestUserDetails.getEmail());
 				user.setPassword(encoded);
-				
+
 				usuarioServiceImpl.updateUsuario(user);
-				
+
 				return new ResponseEntity<Object>(user, HttpStatus.OK);
-			}
-			else
-			{
-				return new ResponseEntity<Object>("You are not authorized to edit other user\'s acounts", HttpStatus.UNAUTHORIZED);
+			} else {
+				return new ResponseEntity<Object>("You are not authorized to edit other user\'s acounts",
+						HttpStatus.UNAUTHORIZED);
 			}
 		}
-	
-		return new ResponseEntity<Object>("The specified user doesn\'t exist in the database, try registering it", HttpStatus.INTERNAL_SERVER_ERROR);
+
+		return new ResponseEntity<Object>("The specified user doesn\'t exist in the database, try registering it",
+				HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 	/**
-	 *  The user will have to login again (JWT Bearer Token is invalid)
+	 * The user will have to login again (JWT Bearer Token is invalid)
 	 */
 	@PreAuthorize("hasRole('ADMIN')")
 	@DeleteMapping(value = "/usuarios/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> eliminarUsuario(@PathVariable(name = "id") Long id) {
 		Usuarios user = usuarioServiceImpl.usuarioById(id);
 		JsonObject json = new JsonObject();
-		
-		if (user != null)
-		{
+
+		if (user != null) {
 			usuarioServiceImpl.deleteUsuario(id);
 			json.addProperty("msg", "User deleted successfully");
 			return new ResponseEntity<String>(this.gson.toJson(json), HttpStatus.OK);
 		}
-		
+
 		json.addProperty("msg", "Error deleting the user");
 		return new ResponseEntity<String>(this.gson.toJson(json), HttpStatus.INTERNAL_SERVER_ERROR);
-		
+
 	}
-	
+
 }
